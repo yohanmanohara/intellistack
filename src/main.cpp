c5540daa4dc8c2be648eef6dfcdab148864b3e4d
yohanmanohara <warushayohan80@gmail.com>
2025-01-27 18:47:21 +0530

    #

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,23 @@
-#include <Arduino.h>
-#include "./stepermotor/stepfunction.h"
+#include <SPI.h>
+#include <MFRC522.h>
 
-// Configuration settings
-const int stepDelay = 200;  // Reduced delay for higher speed
-const int stepCount = 3000; // Number of steps per cycle
+#define SS_PIN 5
+#define RST_PIN 22
+
+MFRC522 mfrc522(SS_PIN, RST_PIN);
 
 void setup() {
-    motorSetup();
+  Serial.begin(9600);
+  SPI.begin();
+  mfrc522.PCD_Init();
+  Serial.println("RFID scanner ready.");
 }
 
 void loop() {
-    // Move forward
-    moveForward(stepCount, stepDelay);
-    delay(1000);  // Wait 1 second
-
-    // Move backward
-    moveBackward(stepCount, stepDelay);
-    delay(1000);  // Wait 1 second
+  if (mfrc522.PICC_IsNewCardPresent()) {
+    Serial.println("Card detected!");
+  } else {
+    Serial.println("No card...");
+  }
+  
 }
